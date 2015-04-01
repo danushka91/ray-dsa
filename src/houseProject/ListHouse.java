4dd00b2ea563db06fe0cbe062f0372a972597777
Rooshan Akthar <rooshan@elakiri.com>
2015-04-01 16:16:39 +0530

    required libraries

--- src/houseProject/ListHouse.java
+++ src/houseProject/ListHouse.java
@@ -1,17 +1,40 @@
 package houseProject;
 
+import org.json.simple.JSONObject;
+
+
 public class ListHouse implements Listable {
 
+private JSONObject houseDetails;
+
+	
+	
+	public ListHouse(JSONObject house){
+		this.houseDetails = house;
+	}
+
+	
 	@Override
-	public int compareTo(Listable anything) {
+	public int compareTo(Listable compareHouse) {
 		// TODO Auto-generated method stub
-		return 0;
+		ListHouse other = (ListHouse) compareHouse;
+		
+		Integer thisInt = Integer.parseInt(this.houseDetails.get("lotNumber").toString());
+		Integer thatInt = Integer.parseInt(other.houseDetails.get("lotNumber").toString());
+		
+		return (thisInt - thatInt);
+		
 	}
 
 	@Override
 	public Listable copy() {
 		// TODO Auto-generated method stub
-		return null;
+		ListHouse result = new ListHouse(houseDetails);
+		return result;
+	}
+	
+	public JSONObject getHouse(){
+		return houseDetails;
 	}
 
 }
